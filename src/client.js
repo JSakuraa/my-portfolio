b743931ee8edaa2421dba18529a2cc0ace4b9417
Justin <jtn8001@gmail.com>
2021-10-18 10:18:09 -0400

    first commit

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import SanityClientConstructor from "@sanity/client"
+import sanityClient from "@sanity/client"
+
+export default SanityClient ({
+    projectId: "dbnnjc3i",
+    dataset: "production"
+})
