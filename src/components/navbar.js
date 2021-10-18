2e886043b22855e81054920fbdc02579011abf33
Justin <jtn8001@gmail.com>
2021-10-18 15:56:40 -0400

    Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,25 @@
 import React from "react";
-
-export default function NavBar() {
-    return <h1>NavBar Page</h1>
-}
+import { NavLink } from "react-router-dom";
+  
+  export default function Example() {
+    return (
+      <header className="bg-gray">
+          <div className="container mx-auto flex justify-between">
+              <nav className="flex">
+                  <NavLink to='' exact className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-orange text-4xl font-bold cursive ">
+                      Justin
+                  </NavLink>
+                  <NavLink to="/about">
+                      About Me
+                  </NavLink>
+                  <NavLink to="/post">
+                      Blog Posts
+                  </NavLink>
+                  <NavLink to="/project">
+                      Projects
+                  </NavLink>
+              </nav>
+          </div>
+      </header>
+    )
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      orange: {
+        DEFAULT: '#FF4C29',
+      },
+      gray: {
+        darkest: '#082032',
+        DEFAULT: '#2C394B',
+        light: '#334756',
+      },
+      white: {
+        DEFAULT: '#F9FAFB',
+      }
+    },
   },
   variants: {
     extend: {},
   },
     plugins: [
       require('@tailwindcss/typography'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/line-clamp'),
-      require('@tailwindcss/aspect-ratio'),
     ]
 }
