ae0e5bbe076d1262bcb86e8fc7b1726515e31461
Justin <jtn8001@gmail.com>
2021-10-19 12:59:24 -0400

    Responsiveness & home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,38 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { SocialIcon } from "react-social-icons";
 
 export default function Example() {
     return (
-        <header className="bg-light">
+        <header className="bg-dark">
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink
                         to=""
                         exact
                         activeClassName="text-white"
-                        className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-orange text-4xl font-bold cursive tracking-widest"
+                        className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-orange text-3xl font-bold cursive tracking-widest lg:text-4xl"
                     >
                         Justin
                     </NavLink>
                     <NavLink
                         to="/about"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-orange"
-                        activeClassName="text-white bg-dark"
+                        className="inline-flex items-center py-2 px-2 my-6 rounded text-white hover:text-orange text-sm lg:text-base"
+                        activeClassName="text-white bg-primary"
                     >
                         About Me
                     </NavLink>
                     <NavLink
                         to="/post"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-orange"
-                        activeClassName="text-white bg-dark"
+                        className="inline-flex items-center py-2 px-2 my-6 rounded text-white hover:text-orange text-sm lg:text-base"
+                        activeClassName="text-white bg-primary"
                     >
                         Blog Posts
                     </NavLink>
                     <NavLink
                         to="/project"
-                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-orange"
-                        activeClassName="text-white bg-dark"
+                        className="inline-flex items-center py-2 px-2 my-6 rounded text-white hover:text-orange text-sm lg:text-base"
+                        activeClassName="text-white bg-primary"
                     >
                         Projects
                     </NavLink>
