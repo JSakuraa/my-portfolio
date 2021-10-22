b477c52e48ee2cae72a65642dd0162eb745c969a
Justin <jtn8001@gmail.com>
2021-10-22 11:19:38 -0400

    Minor formatting

--- src/components/Project.js
+++ src/components/Project.js
@@ -33,50 +33,56 @@ export default function Project() {
           Below you can find all of my work
         </h2>
         <section className="grid md:grid-cols-2 gap-8">
-          {projectData && projectData.map((project, index) => (
-          <article className="relative rounded-lg shadow-xl bg-dark p-16">
-            <h3 className="text-white text-3xl font-bold mb-2 hover:text-orange">
-              <a
-                href={project.link}
-                alt={project.title}
-                target="_blank"
-                rel="noopener noreffer"
-              >
-                {project.title}
-              </a>
-            </h3>
-            <div className="text-white text-xs space-x-4">
-              <span>
-                <strong className="font-bold">Finished on</strong>:{" "}
-                {new Date(project.date).toLocaleDateString()}
-              </span>
-              <span>
-                <strong className="font-bold">Company</strong>: {project.place}
-              </span>
-              <span>
-                <strong className="font-bold">Type</strong>:{" "}
-                {project.projectType}
-              </span>
-              <p className="my-6 text-lg text-white leading-relaxed">
-                {project.description}
-              </p>
-              <a
-                href={project.link}
-                rel="noopener noreffer"
-                target="_blank"
-                className="text-orange text-bold hover:underline text-xl"
-              >
-                Project Site{" "}
-                <span role="img" aria-label="right-pointer">
-                  👉
-                </span>
-              </a>
-              <Link to={"/project/" + project.slug.current} key={project.slug.current}>
-                <span className="text-orange text-bold hover:underline text-xl">Learn More</span>
-              </Link>
-            </div>
-          </article>
-          ))}
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-dark p-16">
+                <Link
+                  to={"/project/" + project.slug.current}
+                  key={project.slug.current}
+                >
+                  <h3 className="text-white text-3xl font-bold mb-2 hover:text-orange">
+                    {project.title}
+                  </h3>
+                </Link>
+                <div className="text-white text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-white leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreffer"
+                    target="_blank"
+                    className="text-orange text-bold hover:underline text-xl"
+                  >
+                    Project Site{" "}
+                    <span role="img" aria-label="right-pointer">
+                      👉
+                    </span>
+                  </a>
+                  <br/>
+                  <Link
+                    to={"/project/" + project.slug.current}
+                    key={project.slug.current}
+                  >
+                    <span className="text-orange text-bold hover:underline text-xl">
+                      Learn More
+                    </span>
+                  </Link>
+                </div>
+              </article>
+            ))}
         </section>
       </section>
     </main>
